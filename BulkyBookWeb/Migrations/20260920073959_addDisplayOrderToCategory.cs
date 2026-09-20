83796d77d07ab647a83268ca11f5d32503233298
aneeshjo <aneeshjose012@gmail.com>
2026-09-20 14:29:00 +0530

    Updated the layout ,Loaded Category to the UI

---
+++ BulkyBookWeb/Migrations/20260920073959_addDisplayOrderToCategory.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBookWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class addDisplayOrderToCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayOrder",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DisplayOrder",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DisplayOrder",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DisplayOrder",
+                value: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayOrder",
+                table: "Categories");
+        }
+    }
+}
--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -7,5 +7,7 @@ namespace BulkyBookWeb.Models
         [Key]
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+
+        public string DisplayOrder { get; set; } = string.Empty;
     }
 }
